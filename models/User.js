ecf300be13c1ebf7bd16ac7d7a11a4d1a0438478
sanjana <sanjanabaskar05@gmail.com>
2025-11-01 11:04:38 +0530

    Switch email sending from Nodemailer to Resend

--- models/User.js
+++ models/User.js
@@ -4,8 +4,8 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  resetPasswordToken: String,
-  resetPasswordExpires: Date,
+  resetToken: String,
+  resetTokenExpire: Date,
 });
 
 export default mongoose.model("User", userSchema);
